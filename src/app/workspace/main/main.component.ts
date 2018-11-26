fc41722345bbe620d13a00c18db00defe33f91b8
liuzhiyu <liuzhiyu@richstonedt.com>
2018-11-26 16:17:06 +0800

    add multi-environment code building configuration

--- src/app/workspace/main/main.component.ts
+++ src/app/workspace/main/main.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NzIconService } from 'ng-zorro-antd';
 import { HttpClient } from '@angular/common/http';
 
+import { environment } from '../../../environments/environment';
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -17,6 +19,7 @@ export class MainComponent implements OnInit {
     this.iconService.fetchFromIconfont({
       scriptUrl: 'https://at.alicdn.com/t/font_931245_uycqwbri2ef.js'
     });
+    console.log(environment.envName);
   }
 
   ngOnInit() {
