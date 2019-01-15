b08d0596d6799c24edb4fd3ec8d75ebcc0a4afb4
BarryLiu1995 <ssdf90771@gmail.com>
2019-01-15 17:27:51 +0800

    modify CanDeactivate router guard sample in asynchronous way on map page

--- src/app/tool/service/dialog.service.ts
+++ src/app/tool/service/dialog.service.ts
@@ -1,15 +1,39 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
+import { NzModalService } from 'ng-zorro-antd';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DialogService {
+  private deactivateObservable: Observable<boolean>;
+  private deactivateObserver: Observer<boolean>;
 
-  constructor() { }
+  constructor(
+    private confirmService: NzModalService
+  ) {
+    this.deactivateObservable = new Observable<boolean>(observer => {
+      this.deactivateObserver = observer;
+    });
+  }
 
-  confirm(message: string = '是否确认离开该页？'): Observable<boolean> {
-    const result = window.confirm(message || 'Is it OK?');
-    return of(result);
+  confirm(): Observable<boolean> {
+    this.confirmService.confirm({
+      nzContent: '确认是否离开该页面？',
+      nzOkText: '是',
+      nzOkType: 'danger',
+      nzCancelText: '否',
+      nzOnOk: () => this.deactivateHandler(true),
+      nzOnCancel: () => this.deactivateHandler(false)
+    });
+    return this.deactivateObservable;
+  }
+
+  /**
+   * 用户是否离开
+   * @param canDeactivate|boolean
+   */
+  private deactivateHandler(canDeactivate: boolean): void {
+    this.deactivateObserver.next(canDeactivate);
   }
 }
